a441debb10f2b9c28438744767cfbb3f1b9b10bd
Vgkmanju <34670259+Vgkmanju@users.noreply.github.com>
2018-01-18 10:45:27 +0530

    Create stringsequence.java

---
+++ assignment/stringsequence.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author skcet
+ */
+public class string_Sequence {
+	
+	public static void usermain(String mainstring,String substring)
+	{
+		if(mainstring.contains(substring))
+			
+		{
+		
+			
+			System.out.println("true");
+		}
+		else
+		{
+			System.out.println("false");
+		}
+	}
+	
+	public static void main(String arg[])
+	{
+		usermain("sriram krishna","sri");
+	}
+	
+
+}
