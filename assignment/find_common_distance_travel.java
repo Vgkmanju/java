33375f7a73ec23c6e72ada213613585d6b4b7d7e
Vgkmanju <34670259+Vgkmanju@users.noreply.github.com>
2018-01-24 09:37:57 -0800

    Create find_common_distance_travel.java

---
+++ assignment/find_common_distance_travel.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x1 = in.nextInt();
+        int v1 = in.nextInt();
+        int x2 = in.nextInt();
+        int v2 = in.nextInt();
+        int dis=x1-x2;
+	int vel=v1-v2;
+	if(v1>v2 && dis%vel==0){
+            System.out.println("YES");
+        }
+        else
+            System.out.println("NO");
+    }
+}
