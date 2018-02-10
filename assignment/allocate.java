50aed999d5ebfa3f2a31751645ce0f60ec737acc
Vgkmanju <34670259+Vgkmanju@users.noreply.github.com>
2018-02-10 10:32:29 +0530

    Create allocate.java

---
+++ assignment/allocate.java
@@ -0,0 +1,20 @@
+public class Allocate {
+    
+  public static void main(String[] args) {
+ 
+    try {
+      long data[] = new long[1000000];
+     
+     
+    }
+    catch (Exception e) {
+      System.out.println(e);
+    }
+ 
+    finally{
+      System.out.println("mandate");
+    }
+  }
+}
+
+
